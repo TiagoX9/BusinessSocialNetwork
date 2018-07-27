e8171e30db181a486412369d51c2d1b9030d422f
Tiago Pereira <tiagojopereira@gmail.com>
2018-07-27 13:30:53 +0200

    Back button on Education

--- devConnector/client/src/components/dashboard/Dashboard.js
+++ devConnector/client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
 import Experience from './Experience';
+import Education from './Education';
 
 class Dashboard extends Component {
     componentDidMount() {
