8fe8171a82e19c863f930e05c28c3d4a40492585
Tiago Pereira <tiagojopereira@gmail.com>
2018-07-31 14:06:40 +0200

    Finished Profile Section and NotFound page

--- devConnector/client/src/App.js
+++ devConnector/client/src/App.js
@@ -22,8 +22,10 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from "./components/not-found/NotFound";
 
 import "./App.css";
+
 
 
 // Check for token
@@ -76,6 +78,7 @@ class App extends Component {
           <Switch>
             <PrivateRoute exact path="/add-education" component={AddEducation} />
           </Switch>
+          <Route exact path="/not-found" component={NotFound} />
           </div>
           <Footer />
         </div>
